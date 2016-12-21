986651c8d1da52bb57b2d869c902f3b69d342aea
Яков Лаптев < >
2016-12-21 20:17:18 +0300

    Добавьте файлы проекта.

---
+++ AdvertisingAgency/Controllers/CustomerController.cs
@@ -0,0 +1,99 @@
+﻿using AdvertisingAgency.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdvertisingAgency.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Entities en = new Entities();
+
+        // GET: Customer
+        public ActionResult Index()
+        {            
+            var c = en.customers.ToList();            
+            return View(c);
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, customer collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                en.customers.Find(id).fio = collection.fio;
+                //en.customers.Find(id).registrationDate = collection.registrationDate;
+                //если нашел такой то меняет по всей базе en.customers.Find(id).email = collection.email;
+                en.customers.Find(id).AspNetUser.UserName = collection.AspNetUser.UserName;
+                en.customers.Find(id).AspNetUser.AspNetRole.Id = en.AspNetRoles.Where(p => p.Name == collection.AspNetUser.AspNetRole.Name).FirstOrDefault().Id;
+                en.Entry(collection).State = System.Data.Entity.EntityState.Modified;
+                en.SaveChanges();
+                return RedirectToAction("../Customer/Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AdvertisingAgency/Controllers/CustomersController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdvertisingAgency.Models;
+
+namespace AdvertisingAgency.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = db.customers.Include(c => c.AspNetUser);
+            return View(customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,fio,registrationDate,email,userId")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email", customer.userId);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email", customer.userId);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,fio,registrationDate,email,userId")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                AspNetUser user = customer.AspNetUser;
+                db.Entry(user).State = EntityState.Modified;
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userId = new SelectList(db.AspNetUsers, "Id", "Email", customer.userId);
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            customer customer = db.customers.Find(id);
+            db.customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
